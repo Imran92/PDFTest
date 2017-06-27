24fc328e72f175b7ac88450d6b768d1d4a973521
imranhossain <imranh920@gmail.com>
2017-06-28 01:15:37 +0600

    Adding all files, working

---
+++ PDFTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PDFTest.Startup))]
+namespace PDFTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
